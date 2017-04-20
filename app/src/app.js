143a55a4d0f1247d3932f50bd7ff6b85f1c3caa6
Andrew Marshall <marshall.a.f@gmail.com>
2017-04-19 20:02:31 -0500

    add react entry

---
+++ app/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const AppUp = () => (
+  <div>App is up!</div>
+);
+
+ReactDOM.render(<AppUp />, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // initialize express
 const app = express();
@@ -8,11 +9,12 @@ app.set('port', (process.env.PORT || 8080));
 // enable parsing of http body
 app.use(bodyParser.urlencoded({extended: false}));
 
+console.log(path.join(__dirname, '../app/static'));
 // set location of static files to serve
 // html and css
-app.use(express.static('../app/static'));
+app.use(express.static(path.join(__dirname, '../app/static')));
 // the bundled javascript
-app.use(express.static('../app/build'));
+app.use(express.static(path.join(__dirname, '../app/build')));
 
 // start the server
 app.listen(app.get('port'), () => {
