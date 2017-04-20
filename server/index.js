19ecde7a72152a9375e3bed4bc1f348bf0248fd4
Andrew Marshall <marshall.a.f@gmail.com>
2017-04-19 19:41:44 -0500

    add static files and simple server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// initialize express
+const app = express();
+app.set('port', (process.env.PORT || 8080));
+
+// enable parsing of http body
+app.use(bodyParser.urlencoded({extended: false}));
+
+// set location of static files to serve
+// html and css
+app.use(express.static('../app/static'));
+// the bundled javascript
+app.use(express.static('../app/build'));
+
+// start the server
+app.listen(app.get('port'), () => {
+  console.log('server listening on port ' + app.get('port'));
+});
