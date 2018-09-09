07de02287846d24518b87ee85bdaae024748bb68
Andrew Marshall <marshall.a.f@gmail.com>
2018-09-09 15:37:09 -0500

    Fix homepage and login formatting

--- app/src/containers/HomePage.jsx
+++ app/src/containers/HomePage.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
+class HomePage extends React.PureComponent {
   render() {
     return (
       <div>
